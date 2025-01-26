1386d3437b34cc22e8a85aafa6c58f810b39442e
Antonio <ajordao14@gmail.com>
2025-01-25 19:41:06 -0500

    Added Comments for Goals of Project.

--- ETL_Console/Program.cs
+++ ETL_Console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello Word!");
+
+//THE GOAL: Utility to use at work for fast SQL ETL and Analysis
+
+//Frog: SQL Read and Write
+
+//Frog: Object Conversion To SQL
+
+//Frog: Json Conversion To SQL
+
+//Tadpole: Unit Tests
+
+//Tadpole: Logging
+
+//Tadpole: Benchmark Time Performance
